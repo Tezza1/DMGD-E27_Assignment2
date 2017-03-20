a8f06c452af0f6508b6aeca5f4939530bc862448
Tezza1 <tshenker@gmail.com>
2017-03-20 12:58:52 +0900

    1st phase summer page

---
+++ html/js/script.js
@@ -0,0 +1,25 @@
+/* Plugin taken from: http://dev.vast.com/jquery-popup-overlay/ */
+$(document).ready(function () {
+
+	// Initialize the plugin
+	$('#my_popup1').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup2').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup3').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup4').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup5').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup6').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup7').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup8').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup9').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup10').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup11').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup12').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup13').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup14').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup15').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup16').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup17').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup18').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup19').popup().css("height", "100%").css("width", "100%");
+	$('#my_popup20').popup().css("height", "100%").css("width", "100%");
+});
